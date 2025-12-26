03519ea627cc1fda765e3edcf0449e49be2421c7
zackzheng1121 <86220968+zackzheng1121@users.noreply.github.com>
2025-12-26 21:04:56 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/222833447.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Zackzheng",
+	bio: "一個寫一些python/c#/一點C++的dev/wmc....總之成分複雜",
 	links: [
 		{
 			name: "Instagram",
