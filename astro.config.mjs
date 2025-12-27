7c91b4d35f8c501ed1b5e3ea9e6c659c6c50dd8a
zackzheng1121 <86220968+zackzheng1121@users.noreply.github.com>
2025-12-27 19:04:57 +0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://zackzheng1121.github.io",
+	site: "https://zack.siiway.org",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
